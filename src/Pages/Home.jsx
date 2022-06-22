6c855339551414d94093b9ca106daf2b17dcc3c6
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-22 17:23:29 +0400

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -69,11 +69,6 @@ export default function Home() {
                     Get started
                   </a>
                 </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                    Copy IP
-                  </button>
-                </div>
               </div>
             </div>
           </main>
