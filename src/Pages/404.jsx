38c6af2feefdbc07e15e6ccca4bd0d4686ec4a7d
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-23 20:37:59 +0400

    Update 404.jsx

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -4,7 +4,7 @@ function PageNotFound() {
   return (
     <>
         <div className="max-w-7xl mx-auto flex items-center justify-center h-screen">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">404 Not Found</h1>
+          <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 sm:text-5xl md:text-6xl ">404 Not Found</h1>
           <div className="Divider"></div>
         </div>
     </>
