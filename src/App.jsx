8d26ac7f08e956dcf11e1ae8a706cb97f8349a62
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-23 12:55:47 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import PageError from "./Pages/404";
 import Navbar from "./Nav";
 import Footer from './Footer';
 import About from './Pages/About.jsx';
+import Announcement from './Announcement';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return(
     <>
       <Navbar />
+      <Announcement />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Servers" element={<Servers />} />
