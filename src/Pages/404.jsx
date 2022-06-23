b83326eaae776f63072856e3cb4f717519d4f65a
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-23 20:27:28 +0400

    Update 404.jsx

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -3,14 +3,10 @@ import React from "react";
 function PageNotFound() {
   return (
     <>
-      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-  <img src="https://play.tailwindcss.com/img/beams.jpg" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
-  <div class="absolute inset-0 bg-[url(https://play.tailwindcss.com/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
         <div className="max-w-7xl mx-auto flex items-center justify-center h-screen">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">404 Not Found</h1>
           <div className="Divider"></div>
         </div>
-    </div>
     </>
   );
 }
