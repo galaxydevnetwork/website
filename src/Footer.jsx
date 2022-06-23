73c7968658bf6cb4282e2cb738056fc2f2f87782
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-23 21:59:01 +0400

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -34,8 +34,8 @@ function Footer() {
                 <li class="mb-4">
                     <a href="https://twitter.com/GLXYDEVNETWORK" class="hover:underline">Twitter</a>
                 </li>
-              <li class="mb-4"><a href="#" class="hover:underline">
-                    <a href="https://discord.gg/pvMdwVhekf" class="hover:underline">Contact Us</a>
+              <li class="mb-4"><a href="https://discord.gg/pvMdwVhekf" class="hover:underline">
+                   Contact us
                 </li>
             </ul>
         </div>
