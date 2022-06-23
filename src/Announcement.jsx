a7471ac32853e8f394a124c1832085fb2a883243
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-23 13:15:38 +0400

    Update Announcement.jsx

--- src/Announcement.jsx
+++ src/Announcement.jsx
@@ -18,7 +18,7 @@ export default function Announcement() {
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
             <a
-              href="#"
+              href="https://blog.galaxynetwork.ml/galaxynetwork-release-date-revealed/"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
               Learn more
