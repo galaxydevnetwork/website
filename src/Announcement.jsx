70dbbe184ccd0f0434cf4c79c54918885ed719f0
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-06-24 18:51:36 +0400

    Update Announcement.jsx

--- src/Announcement.jsx
+++ src/Announcement.jsx
@@ -12,13 +12,13 @@ export default function Announcement() {
               <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </span>
             <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">GalaxyNetwork coming in July</span>
-              <span className="hidden md:inline">Big news! We're excited to announce that GalaxyNetwork is coming in July</span>
+              <span className="md:hidden">GalaxyNetwork Released</span>
+              <span className="hidden md:inline">Big news! We're excited to announce that GalaxyNetwork is now releasing</span>
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
             <a
-              href="https://blog.galaxynetwork.ml/galaxynetwork-release-date-revealed/"
+              href="https://blog.galaxynetwork.ml/galaxynetwork-new/"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
               Learn more
