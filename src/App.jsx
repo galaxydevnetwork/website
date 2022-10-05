24f3e7edac816ca0e97fd829336075bfd6cf47e3
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-10-05 22:57:09 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './index.css';
-import Home from "./Pages/Home";
-import Projects from "./Pages/Projects";
-import PageError from "./Pages/404";
-import Navbar from "./Nav";
-import Footer from './Footer';
+import Home from "./Pages/Home.jsx";
+import Projects from "./Pages/Projects.jsx";
+import PageError from "./Pages/404.jsx";
+import Navbar from "./Nav.jsx";
+import Footer from './Footer.jsx';
 import About from './Pages/About.jsx';
 import { Routes, Route } from "react-router-dom";
 
