9276ec688ab2969becc115a054c150eb50720c8f
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-10-06 08:44:43 +0400

    commit message

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,126 +1,102 @@
 import React, { useState } from "react";
-import { Transition } from "@headlessui/react";
 
 function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <nav className="sticky top-0 bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <p className="text-white">GalaxyDevNetwork</p>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="/"
-                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Home
-                  </a>
-
-                  <a
-                    href="/Servers"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Projects
-                  </a>
-                  <a
-                    href="/About"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    About
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
+    <>
+      <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+              <a href="/" class="flex items-center">
+                  <img src="https://raw.githubusercontent.com/galaxydevnetwork/website/main/galaxynetwork.png" class="mr-3 h-6 sm:h-9" alt="GalaxyDevNetwork Logo"></img>
+                  <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">GalaxyDevNetwork</span>
+              </a>
+              <button data-collapse-toggle="mega-menu-full" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
+                      <span class="sr-only">Open main menu</span>
+                      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
               </button>
-            </div>
-          </div>
-        </div>
-
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="/"
-                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Home
-                </a>
-
-                <a
-                  href="Servers"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="About"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  About
-                </a>
-
+              <div id="mega-menu-full" class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
+                  <ul class="flex flex-col mt-4 text-sm font-medium md:flex-row md:space-x-8 md:mt-0">
+                      <li>
+                          <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
+                      </li>
+                      <li>
+                          <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown" class="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Links <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                      </li>
+                      <li>
+                          <a href="projects" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
+                      </li>
+                      <li>
+                          <a href="about" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                      </li>
+                  </ul>
               </div>
-            </div>
-          )}
-        </Transition>
+          </div>
+          <div id="mega-menu-full-dropdown" class="mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600">
+              <div class="grid py-5 px-4 mx-auto max-w-screen-xl text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+                  <ul aria-labelledby="mega-menu-full-dropdown-button">
+                      <li>
+                          <a href="/" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDevNetwork</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">It's this website, totally this website</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="https://galaxynetwork.page.link/discord" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDevNetwork Discord</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">The discord server with all projects made from GalaxyDevNetwork into one!</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="https://blog.galaxynetwork.ml" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDevNetwork Blog</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">Blogs that are made in GalaxyDevNetwork about new updates about our projects</span>
+                          </a>
+                      </li>
+                  </ul>
+                  <ul>
+
+                      <li>
+                          <a href="https://www.meme20200.tk" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">meme20200</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">meme20200 A Advanced Minehut Dupe SMP</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="https://teams.meme20200.tk" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">meme20200 Teams</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">Create Teams within meme20200 Minehut Server and view it here!</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="https://meme20200.fandom.com" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">meme20200 Wiki</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">Learn more about meme20200 here!</span>
+                          </a>
+                      </li>
+                  </ul>
+                  <ul class="hidden md:block">
+                      <li>
+                          <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDev</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">The discord bot which came from the galaxy neighbourhood</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDev Blogs</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">Blogs that are made from the devel0per who made it</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                              <div class="font-semibold">GalaxyDev Docs</div>
+                              <span class="text-sm font-light text-gray-500 dark:text-gray-400">Learn the powers of this discord bot</span>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
       </nav>
-    </div>
+    </>
   );
 }
 
