591dd18db5399a555994f97d69b40c310e502e76
Galaxy202 <58356708+Galaxy202@users.noreply.github.com>
2022-10-12 06:52:32 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import './index.css';
-import Home from "./Pages/Home.jsx";
-import Projects from "./Pages/Projects.jsx";
-import PageError from "./Pages/404.jsx";
-import Navbar from "./Nav.jsx";
-import Footer from './Footer.jsx';
-import About from './Pages/About.jsx';
-import { Routes, Route } from "react-router-dom";
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home.jsx";
+import DrawerAppBar from "./components/Nav.jsx";
 
-function App() {
-  return(
+export default function App() {
+  return (
     <>
-      <Navbar />
+      <DrawerAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/*" element={<PageError />} />
       </Routes>
-      <Footer />
     </>
-    )
+  );
 }
-
-export default App;
